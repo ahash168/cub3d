d1c4cf708933854011a5b9fccf22b64b6c0500a4
Tarek Badawi <tabadawi@student.42abudhabi.ae>
2024-10-14 21:55:04 +0400

    js an attempt on floor shading

--- srcs/execution/raaaaays.c
+++ srcs/execution/raaaaays.c
@@ -6,7 +6,7 @@
 /*   By: tabadawi <tabadawi@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 18:43:53 by ahashem           #+#    #+#             */
-/*   Updated: 2024/10/13 22:06:17 by tabadawi         ###   ########.fr       */
+/*   Updated: 2024/10/14 21:54:30 by tabadawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 #define P3	3 * PI/2
 #define WINDOW_W 1920
 #define WINDOW_H 1080
-#define MINIMUM 200
-#define MAXIMUM 400
+#define MINIMUM 50
+#define MAXIMUM 250
 
 int	change_shade(int colour, float dist)
 {
@@ -47,14 +47,57 @@ int	change_shade(int colour, float dist)
 	return (shaded_color);
 }
 
+int	shade_floor(int colour, int y, int window_height, int final)
+{
+	float shade_factor = 0.0;
+	float minimum = 50.0;
+	
+	if (window_height - y < minimum)
+		shade_factor = 1.0;
+	else if (window_height - y > final)
+		shade_factor = 0.0;
+	else
+		shade_factor = 1.0 - ((y - minimum) / (final - minimum));
+
+	if (shade_factor < 0.0f)
+        shade_factor = 0.0f;
+    if (shade_factor > 1.0f)
+        shade_factor = 1.0f;
+
+	unsigned int trgb = colour;
+	unsigned char r = (trgb >> 16) & 0xFF;
+	unsigned char g = (trgb >> 8) & 0xFF;
+	unsigned char b = trgb & 0xFF;
+
+	r = (unsigned char)(r * shade_factor);
+	g = (unsigned char)(g * shade_factor);
+	b = (unsigned char)(b * shade_factor);
+
+	unsigned int shaded_color = (trgb & 0xFF000000) | (r << 16) | (g << 8) | b;
+	return (shaded_color);
+}
+
 void	draw_vertical_line(t_game *game, int x, float h, int window_height, int color)
 {
 	int	y;
 
+	int i = 0;
+	int final;
 	y = (window_height - h) / 2;
+	while (i < y)
+	{
+		pixel_put(&game->img, x, i, game->textures.ceiling);
+		i++;
+	}
 	while ((y < window_height - ((window_height - h ) / 2)) && y >= 0)
 	{
 		pixel_put(&game->img, x, y, color);
+		y++;
+	}
+	final = y;
+	while (y < window_height)
+	{
+		pixel_put(&game->img, x, y, shade_floor(game->textures.floor, y, 1080, final));
 		y++;
 	}
 }
@@ -209,30 +252,32 @@ void raaaaays(t_game *game)
 		ray_angle -= (2 * PI);
 	offset = fov_rd / WINDOW_W;
 	float final;
-	float ca;
+	float no_fishy;
 	int colour;
 	float pierce = tan(fov_rd / 2.00);
 	while (ray < WINDOW_W)
 	{
-		ca = game->map.angle - ray_angle;
-		if (ca < 0)
-			ca += (2 * PI);
-		else if (ca > (2 * PI))
-			ca -= (2 * PI);
+		no_fishy = game->map.angle - ray_angle;
+		if (no_fishy < 0)
+			no_fishy += (2 * PI);
+		else if (no_fishy > (2 * PI))
+			no_fishy -= (2 * PI);
 		h_inter = get_h_inter(game, ray_angle, &hx, &hy);
 		v_inter = get_v_inter(game, ray_angle, &vx, &vy);
 		if (v_inter < h_inter)
 		{
-			final = v_inter * cos(ca);
-			colour = 0x00b8bfc2;
+			final = v_inter * cos(no_fishy);
+			colour = 0x9c9358;
+			// colour = 0x00b8bfc2;
 			// colour *= v_inter / 100;
 			colour = change_shade(colour, final);
 			// draw_line(game, (game->map.player_x * 64), (game->map.player_y * 64), vx, vy, 0x2a9df5);
 		}
 		else
 		{
-			final = h_inter * cos(ca);
-			colour  = 0x002a9df5;
+			final = h_inter * cos(no_fishy);
+			colour = 0x9c9358;
+			// colour  = 0x002a9df5;
 			// colour *= h_inter / 100;
 			colour = change_shade(colour, final);
 			// draw_line(game, (game->map.player_x * 64), (game->map.player_y * 64), hx, hy, 0x2a9df5);
