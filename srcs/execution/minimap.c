c8f4535813ed8144b707f620d1c2facacf9dc144
Tarek Badawi <tabadawi@student.42abudhabi.ae>
2024-10-20 17:09:54 +0400

    working minimap

--- srcs/execution/minimap.c
+++ srcs/execution/minimap.c
@@ -6,126 +6,176 @@
 /*   By: tabadawi <tabadawi@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 12:24:35 by tabadawi          #+#    #+#             */
-/*   Updated: 2024/10/14 13:18:01 by tabadawi         ###   ########.fr       */
+/*   Updated: 2024/10/20 16:38:01 by tabadawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
-
-void draw_circle(t_game *game, int x_center, int y_center, int color)
+void ft_swap(int *a, int *b)
 {
-    int x = 8;
-    int y = 0;
-    int decisionOver2 = 1 - x;
-
-    while (y <= x)
-    {
-        pixel_put(&game->img, x_center + x, y_center + y, color);
-        pixel_put(&game->img, x_center + y, y_center + x, color);
-        pixel_put(&game->img, x_center - x, y_center + y, color);
-        pixel_put(&game->img, x_center - y, y_center + x, color);
-        pixel_put(&game->img, x_center - x, y_center - y, color);
-        pixel_put(&game->img, x_center - y, y_center - x, color);
-        pixel_put(&game->img, x_center + x, y_center - y, color);
-        pixel_put(&game->img, x_center + y, y_center - x, color);
-        
-        y++;
-        if (decisionOver2 <= 0)
-        {
-            decisionOver2 += 2 * y + 1;
-        }
-        else
-        {
-            x--;
-            decisionOver2 += 2 * (y - x) + 1;
-        }
-    }
+    int	temp;
+	
+	temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int interpolate(int y, int y1, int y2, int x1, int x2)
+{
+    if (y2 == y1)
+		return (x1);// Avoid division by zero
+	return (x1 + (x2 - x1) * (y - y1) / (y2 - y1));
 }
 
-// Draw a line to represent the direction vector
-void draw_direction_vector(t_game *game, int x_center, int y_center, float angle, int color)
+void fill_triangle(t_game *game, int x1, int y1, int x2, int y2, int x3, int y3, int color)
 {
-    // Use angle to calculate the endpoint of the line
-    int x_end = x_center + cos(angle) * 10;
-    int y_end = y_center + sin(angle) * 10;
-
-    // Draw a simple line using Bresenham's algorithm
-    int dx = abs(x_end - x_center);
-    int dy = abs(y_end - y_center);
-    int sx = (x_center < x_end) ? 1 : -1;
-    int sy = (y_center < y_end) ? 1 : -1;
-    int err = dx - dy;
-    
-    while (x_center != x_end || y_center != y_end)
-    {
-        pixel_put(&game->img, x_center, y_center, color);
-        int e2 = err * 2;
-        if (e2 > -dy)
-        {
-            err -= dy;
-            x_center += sx;
-        }
-        if (e2 < dx)
-        {
-            err += dx;
-            y_center += sy;
-        }
-    }
-}
-
-void	draw_player(t_game *game, int x, int y, int colour)
-{
-	draw_circle(game, x, y, colour);
-	draw_direction_vector(game, x, y, game->map.angle, colour);
-}
-
-void	draw_wall(t_game *game, int x, int y, int colour)
-{
-	int j;
-	int i  = y;
-	while (++i < y + 63)
+    // Sort vertices by y-coordinate (y1 <= y2 <= y3)
+    if (y2 < y1)
 	{
-		j = x - 1;
-		while (++j < x + 63)
-			pixel_put(&game->img, j, i, colour);
+		ft_swap(&x1, &x2);
+		ft_swap(&y1, &y2);
+	}
+	if (y3 < y1)
+	{
+		ft_swap(&x1, &x3);
+		ft_swap(&y1, &y3);
+	}
+    if (y3 < y2)
+	{
+		ft_swap(&x2, &x3);
+		ft_swap(&y2, &y3);
+	}
+    // Draw the upper part of the triangle
+	if (y2 != y1)
+	{
+    	for (int y = y1; y <= y2; y++)
+    	{
+    	    int x_start = interpolate(y, y1, y2, x1, x2);
+    	    int x_end = interpolate(y, y1, y3, x1, x3);
+			if (x_start > x_end)
+				ft_swap(&x_start, &x_end);
+    	    for (int x = x_start; x <= x_end; x++)
+    	        pixel_put(&game->img, x, y, color);
+    	}
+	}
+    // Draw the lower part of the triangle
+	if (y3 != y2)
+    {
+    	for (int y = y2; y <= y3; y++)
+    	{
+    	    int x_start = interpolate(y, y2, y3, x2, x3);
+    	    int x_end = interpolate(y, y1, y3, x1, x3);
+			if (x_start > x_end)
+				ft_swap(&x_start, &x_end);
+    	    for (int x = x_start; x <= x_end; x++)
+    	        pixel_put(&game->img, x, y, color);
+    	}
 	}
 }
 
-void	draw_floor(t_game *game, int x, int y, int colour)
+void draw_filled_arrow(t_game *game, int x_center, int y_center, float angle, int size, int color)
 {
-	int j;
-	int i  = y;
-	while (++i < y + 63)
+    // Calculate the three points of the arrow (triangle)
+    // The tip is placed at a certain distance from the center
+    int tip_x = x_center + cos(angle) * size;
+    int tip_y = y_center + sin(angle) * size;
+
+    // Base points of the arrow are calculated at an equal distance from the center
+    int base_left_x = x_center + cos(angle + M_PI_2) * (size / 2);
+    int base_left_y = y_center + sin(angle + M_PI_2) * (size / 2);
+
+    int base_right_x = x_center + cos(angle - M_PI_2) * (size / 2);
+    int base_right_y = y_center + sin(angle - M_PI_2) * (size / 2);
+
+    // Adjusting the center of the triangle (the player) to be the average of the points
+    int center_x = (tip_x + base_left_x + base_right_x) / 3;
+    int center_y = (tip_y + base_left_y + base_right_y) / 3;
+
+    // Translate the points so the player's position is at the specified center
+    int dx = x_center - center_x;
+    int dy = y_center - center_y;
+
+    tip_x += dx;
+    tip_y += dy;
+    base_left_x += dx;
+    base_left_y += dy;
+    base_right_x += dx;
+    base_right_y += dy;
+
+    // Fill the triangle defined by the three points
+    fill_triangle(game, tip_x, tip_y, base_left_x, base_left_y, base_right_x, base_right_y, color);
+}
+
+int	in_bounds(t_game *game, int x, int y)
+{
+	if ((x / 40) >= 0 \
+	&& (y / 40) >= 0 \
+	&& (x / 40) < game->map.width \
+	&& (y / 40) < game->map.height)
+		return (1);
+	return (0);
+}
+
+void	draw_small(t_game *game, int x, int y, int draw_x, int draw_y)
+{
+	if (in_bounds(game, x, y))
 	{
-		j = x - 1;
-		if (i == y + 1 || i == y + 62)
-			while (++j < x + 63)
-				pixel_put(&game->img, j, i, colour);
+		if (game->map.map[y / 40][x / 40] == '1')
+			pixel_put(&game->img, draw_x, draw_y, 0x9c9358);
+		else if (game->map.map[y / 40][x / 40] == ' ')
+			pixel_put(&game->img, draw_x, draw_y, 0x000000);
 		else
+			pixel_put(&game->img, draw_x, draw_y, 0x000000);
+	}
+	else
+		pixel_put(&game->img, draw_x, draw_y, 0x000000);
+}
+
+void	draw_border(t_game *game)
+{
+	int start_x = 40;
+	int start_y = 40;
+	
+	int i = start_y;
+	int j = start_x;
+	while (i < start_y + 320)
+	{
+		j = start_x;
+		while (j < start_x + 320)
 		{
-			pixel_put(&game->img, j + 1, i, colour);
-			pixel_put(&game->img, j + 62, i, colour);
+			pixel_put(&game->img, j, i, 0xffffff);
+			if ((j == start_x + 10) && ((i > start_y + 10) && (i < start_y + 310)))
+				j += 300;
+			else
+				j++;
 		}
+		i++;
 	}
 }
 
-void	draw_map(t_game *game)
+void    draw_minimap(t_game *game)
 {
-	int i = -1;
-	int j;
-	while (game->map.map[++i])
+	int i, j, x, y;
+	int draw_x, draw_y;
+	i  = game->map.player_y * 40;
+	j  = game->map.player_x * 40;
+	
+	y = i - 150;
+	draw_y = 50;
+	while (y < i + 150)
 	{
-		j = -1;
-		while (game->map.map[i][++j])
+		draw_x = 50;
+		x = j - 150;
+		while (x < j + 150)
 		{
-			if (game->map.map[i][j] == '1')
-				draw_wall(game, j * 64, i * 64, 0xb8bfc2);
-			else if (game->map.map[i][j] == ' ')
-				draw_wall(game, j * 64, i * 64, 0x242424);
-			else
-				draw_floor(game, j * 64, i * 64, 0xf7adbc);
+			draw_small(game, x, y, draw_x, draw_y);
+			x++;
+			draw_x++;
 		}
-	}	
+		y++;
+		draw_y++;
+	}
+	draw_border(game);
+	draw_filled_arrow(game, 200, 200, game->map.angle, 20, 0xFFFFFF);
 }
 
 void	rendermap(t_game *game)
@@ -135,7 +185,7 @@ void	rendermap(t_game *game)
 	// draw_map(game);
 	// draw_player(game, game->map.player_x * 64, game->map.player_y * 64, 0x2a9df5);
     raaaaays(game);
+    draw_minimap(game);
 	mlx_put_image_to_window(game->mlx, game->window, game->img.img, 0, 0);
 	mlx_destroy_image(game->mlx, game->img.img);
 }
-
